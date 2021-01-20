ee3cc19f9948a260994fd1d8d2312ca9f89cfe40
johnny <johnnyjojoa7@gmail.com>
2021-01-20 12:41:19 -0500

    modulos-paquetes

---
+++ paquetes/__init__.py
@@ -0,0 +1,6 @@
+#Para que una carpeta funciones como un paquete se debe crear dentro un archivo con nombre __init__.py
+
+#Los paquetes son directorios donde se almacenarán módulos relacionados entre si
+
+#Sirven para organizar y reutilizar los módulos
+
---
+++ utilizar_modulo_del_paquete_calculos.py
@@ -0,0 +1,8 @@
+from paquetes.calculos import dividir
+
+dividir(2,1)
+
+from paquetes.redondeo_potencia.redondeo_potencia import redondear #import from nombre_del_paquete.modulo
+
+redondear(2.1)
+
